71994ccfbeaa7209745fb3a4d00958e7df8c9d01
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-12-02 05:21:25 +0300

    Implement language switch functionality in registration form

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
+
+// Language switch functionality
+const languageBtn = document.getElementById('languageBtn');
+if (languageBtn) {
+    languageBtn.addEventListener('click', function() {
+        const currentPage = window.location.pathname.split('/').pop();
+        if (currentPage === 'index.html') {
+            window.location.href = 'en_index.html';
+        } else if (currentPage === 'en_index.html') {
+            window.location.href = 'index.html';
+        }
+    });
+}
